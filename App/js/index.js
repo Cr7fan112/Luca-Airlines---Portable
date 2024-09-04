e9e359c1c45ae9b69f1dd3cb8c9bcf3063cbe894
Cr7fan112 <Minceraft1@web.de>
2024-09-04 09:28:35 +0200

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -1,47 +1,53 @@
-// The default code is a module class (inherited from xui.Module)
-// Ensure that all the value of "key/value pair" does not refer to external variables
 xui.Class('App', 'xui.Module',{
     Instance:{
-        // Dependency classes
         Dependencies:[],
-        // Required modules
         Required:[],
 
-        // To initialize properties
         properties : {},
 
-        // To initialize instance(e.g. properties)
         initialize : function(){
         },
 
-        // To initialize internal components (mostly UI controls)
-        // *** If you're not a skilled, dont modify this function manually ***
         iniComponents : function(){
-            // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
+            
+            // Adding a heading for "Luca Airlines Portable"
+            append(
+                xui.create("xui.UI.Label")
+                .setHost(host,"xui_ui_label")
+                .setLeft("1em")
+                .setTop("1em")
+                .setWidth("25em")
+                .setHeight("2.5em")
+                .setCaption("Luca Airlines Portable")
+                .setHAlign("center")
+                .setVAlign("middle")
+                .setFontSize("2em")
+                .setFontWeight("bold")
+                .setColor("#FFFFFF")
+                .setBackground("#007BFF")
+                .setBorderRadius("10px")
+                .setPadding("0.5em")
+            );
 
+            // Adding the image below the heading
+            append(
+                xui.create("xui.UI.Image")
+                .setHost(host,"xui_ui_image5")
+                .setLeft("7em")
+                .setTop("5em")
+                .setWidth("20em")
+                .setHeight("15em")
+                .setSrc("{xui.ini.img_pic}")
+                .setShadow(true)
+                .setBorderRadius("10px")
+            );
+            
             return children;
-            // ]]Code created by CrossUI RAD Studio
         },
 
-        // Give a chance to determine which UI controls will be appended to parent container
         customAppend : function(parent, subId, left, top){
-            // "return false" will cause all the internal UI controls will be added to the parent panel
             return false;
         }
-        /*,
-        // To determine how properties affects this module
-        propSetAction : function(prop){
-        },
-        // To set all node's style in this modlue
-        customStyle:{}
-    },
-    //To customize the default properties and event handlers
-    Static:{
-        $DataModel:{
-        },
-        $EventHandlers:{
-        }
-    */
     }
 });
